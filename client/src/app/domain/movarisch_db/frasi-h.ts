a551aeb85a143f23f3d1c5707d04b85ac12753a3
matteferrag <ing.ferraguti@gmail.com>
2026-01-03 15:44:46 +0100

    Remove Skaffolder-CLI references

--- client/src/app/domain/movarisch_db/frasi-h.ts
+++
@@ -1,9 +0,0 @@
-import { FrasiHBase } from './base/frasi-h.base';
-
-/**
- * YOU CAN OVERRIDE HERE FrasiHBase.ts
- */
-export class FrasiH extends FrasiHBase {
-
-    // Insert here your custom attributes and function
-}
--- client/src/app/domain/movarisch_db/processo.ts
+++
@@ -1,9 +0,0 @@
-import { ProcessoBase } from './base/processo.base';
-
-/**
- * YOU CAN OVERRIDE HERE ProcessoBase.ts
- */
-export class Processo extends ProcessoBase {
-
-    // Insert here your custom attributes and function
-}
