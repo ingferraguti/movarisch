8dbce472bcde91c5857fdd525b77b13874ab26f5
ingferraguti <ing.ferraguti@gmail.com>
2026-01-12 15:33:31 +0100

    bugfix

--- api/action.php
+++ api/action.php
@@ -7,6 +7,27 @@ require 'security/Security.php';
 //init Slim Framework
 \Slim\Slim::registerAutoloader();
 $app = new \Slim\Slim();
+
+// ---- CORS (DEV) ----
+$origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '*';
+
+// Se vuoi essere più restrittivo, metti esplicitamente:
+// $origin = 'http://localhost:3000';
+
+$app->hook('slim.before', function () use ($app, $origin) {
+    $res = $app->response();
+    $res->headers->set('Access-Control-Allow-Origin', $origin);
+    $res->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    $res->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+    $res->headers->set('Access-Control-Max-Age', '86400');
+});
+
+// Catch-all preflight
+$app->options('/(:name+)', function () use ($app) {
+    $app->response()->setStatus(200);
+});
+
+
 $app->add(new \Security($app));
 require 'security/Login.php';
 require 'security/ManageUser.php';
